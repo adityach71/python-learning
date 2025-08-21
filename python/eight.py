b1f360c177da37b0bdb2d8a2d80dc5104685b7fd
adityach71 <minikakumari8420@gmail.com>
2025-08-21 23:19:25 +0530

    eight

---
+++ python/eight.py
@@ -0,0 +1,17 @@
+name = "Aditya,Harry"
+print(name[0:6])
+print(len(name))
+
+fruit= "Mango"
+len1 = len(fruit)
+print("Mango ia a", len1,"letter word")
+
+print(fruit[0:4]) # including 0 but not the 4
+print(fruit[1:4]) # including 1 but not the 4
+print(fruit[:5])
+print(fruit[0:-3])
+print(fruit[0:len(fruit)-3])
+print(fruit[-1:len(fruit)-3])
+print(fruit[-3:-1])
+nm = "Harry"
+print(nm[-4:-2])
