d593e19f81c4bbb5f1d6122b5bcd1d499a901f27
adityach71 <minikakumari8420@gmail.com>
2025-08-30 19:39:45 +0530

    good

---
+++ python/11 EX-2.py
@@ -0,0 +1,15 @@
+import time
+
+t = time.strftime('%H:%M:%S')
+hour = int(time.strftime('%H'))  
+
+if hour >= 0 and hour < 12:
+    print("Good Morning Sir!")
+elif hour >= 12 and hour < 17:
+    print("Good Afternoon Sir!")
+elif hour >= 17 and hour < 24:
+    print("Good Night Sir!")
+
+
+
+# https://docs.python.org/3/library/time.html#time.strftime
