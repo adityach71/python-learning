8df934067a3a164cfe429457c01c504228641b57
adityach71 <minikakumari8420@gmail.com>
2025-09-04 23:17:21 +0530

    f-strings done

---
+++ python/23_f-strings.py
@@ -0,0 +1,10 @@
+letter = "Hey my name is {1} and I am from {2}"
+country = "India"
+name = "Aditya"
+
+# print(letter.format(country, name))
+print(f"Hey my name is {name} and I am from {country}")
+
+price = 49.09999
+txt = f"for only {price:.2f} dollars!"
+print(txt)
